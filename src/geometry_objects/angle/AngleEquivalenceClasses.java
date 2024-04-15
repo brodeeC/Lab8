eddf2d1786c872fa6dc63020db4e1a2506f2485d
brodeeC <brodeeclontz@gmail.com>
2024-04-15 16:02:41 -0400

    AngleEQClass

--- src/geometry_objects/angle/AngleEquivalenceClasses.java
+++ src/geometry_objects/angle/AngleEquivalenceClasses.java
@@ -1,9 +1,12 @@
 package geometry_objects.angle;
 
+import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.List;
 
 import geometry_objects.angle.comparators.AngleStructureComparator;
 import utilities.EquivalenceClasses.*;
+import utilities.LinkedEquivalenceClass.LinkedEquivalenceClass;
 
 /**
  * Given the figure below:
@@ -28,5 +31,5 @@ public class AngleEquivalenceClasses extends EquivalenceClasses<Angle>
 	public AngleEquivalenceClasses(AngleStructureComparator comp) {
 		super(comp);
 	}
-	
+
 }
